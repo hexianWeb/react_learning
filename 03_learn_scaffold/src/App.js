4e805daae36ccf9373b0896c64437d240d4a2e13
hexianWeb <2387213640@qq.com>
2022-06-23 17:06:52 +0800

    6/23

---
+++ 03_learn_scaffold/src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      counter: 0,
+    };
+  }
+  render() {
+    return (
+      <div>
+        <h2>当前 {this.state.counter}</h2>
+        <button>+</button>
+        <button>-</button>
+      </div>
+    );
+  }
+}
